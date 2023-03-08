49ed8940bb972a10768e508be46692daf1b61bb0
xiuyu0000 <hujingsong4@huawei.com>
2023-03-08 17:37:54 +0800

    finish load_local_model api

--- mindhub/models/registry.py
+++ mindhub/models/registry.py
@@ -1,28 +1,57 @@
+import os
+import sys
+from importlib.util import spec_from_file_location, module_from_spec
+
+from typing import Generic
+
+from mindhub.utils.download import DownLoad
+from mindhub.env import GITHUB_REPO_URL
 
 __all__ = ['register_model_info',
            'list_models']
 
 _local_models = dict()
 
-def register_model_info(
-                       model_name: str,
-                       paper: str = "",
-                       type: str = "",
-                       ):
-    '''
+
+def load_local_model(module_name: str,
+                     module_local_path: str,
+                     **kwargs):
+
+    sys.path.insert(0, module_local_path)
+
+    # Load the module from the specified file path
+    spec = spec_from_file_location(module_name, os.path.join(module_local_path, "mindspore_hub_conf.py"))
+    model_module = module_from_spec(spec)
+    spec.loader.exec_module(model_module)
+
+    model = model_module[module_name]
+
+    sys.path.pop(0)
+
+    return model
+
+
+def register_model_info(model_name: str,
+                        pretrained: bool = False,
+                        paper: str = "",
+                        model_type: str = "",
+                        ):
+    """
     将需要执行的类进行注册.
-    
+
     Args:
        model_name(str): 需要注册的模型名称+规格+数据集.
+       pretrained(bool): 是否存在可加载的预训练模型.
        paper(str): 模型的原论文名称，方便用户区分简称相同的不同模型。
-       type(str): 所属领域/子领域
-    '''
+       model_type(str): 所属领域/子领域
+    """
 
     # 注册模型信息
-    def _wrapper():
-        return 0
+    def _wrapper(cls: Generic) -> Generic:
+       pass
 
     return _wrapper
+
 
 def list_models(
                filter: str,
---
+++ resource/local_model_demo.py
@@ -0,0 +1,21 @@
+
+if __name__ == "__main__":
+
+    import importlib.util
+    import sys
+
+    sys.path.insert(0, "/data1/hjs/mindhub/resource/tinydarknet_imagenet/")
+
+    # Load the module from the specified file path
+    spec = importlib.util.spec_from_file_location("TinyDarkNetImageNet", "/data1/hjs/mindhub/resource/tinydarknet_imagenet/mindspore_hub_conf.py")
+    model_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(model_module)
+
+    model = model_module.CrossEntropySmooth()
+
+    print(model)
+
+    sys.path.pop(0)
+
+    # Get the model object from the module
+
