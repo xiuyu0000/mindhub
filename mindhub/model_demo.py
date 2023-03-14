71ec3a79781ff4a2ff4657bcc7ce7adac1610846
xiuyu0000 <hujingsong4@huawei.com>
2023-03-14 22:54:25 +0800

    delete modules test demo

--- mindhub/model_demo.py
+++
@@ -1,5 +0,0 @@
-
-if __name__ == "__main__":
-    from mindhub.models.model import Model
-    model = Model("tinydarknet_imagenet", "/data1/hjs/mindhub_repo/resource/tinydarknet_imagenet", pretrained=True)
-    # model = Model("tinydarknet_imagenet", pretrained=True)
