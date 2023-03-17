544044872e22241f5ef3203b79c7b3ee1f09cafe
xiuyu0000 <110210055+xiuyu0000@users.noreply.github.com>
2023-03-17 11:34:11 +0800

    Delete model_demo.py

--- model_demo.py
+++
@@ -1,9 +0,0 @@
-
-if __name__ == "__main__":
-    import os
-    from mindhub.models.model import Model
-
-    net = Model("tinydarknet_imagenet", "/data1/hjs/mindhub/resource/tinydarknet_imagenet/", pretrained=True)
-    print(net.infer("/data1/tinydarknet/data/infer/n02090622/",
-                    os.path.join("/data1/hjs/mindhub/resource/tinydarknet_imagenet/",
-                                 "./label_map.json")))
